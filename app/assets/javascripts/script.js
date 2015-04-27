779ae9ab91dcf92783a553c3fc78160962f7d457
Gary Tsai <gary.m.tsai@gmail.com>
2015-04-27 15:08:41 -0700

    adds new files

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,15 @@
+// Didn't complete in time
+
+// var taskList= {
+//   init: function() {
+//     $('.projects-holder').on("ajax:success", this.updateTasks)
+//   },
+//   updateTasks: function(event, data, status, xhr){
+//     $('.delete').remove()
+//     $('#project-form').append(data.task_list)
+//   }
+// }
+
+// $(document).ready(function(){
+//   taskList.init();
+// })
